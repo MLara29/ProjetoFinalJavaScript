e5b6fae73a123ea5df8db13a930ae84f49630e03
Guilherme Ferreira <gui_henrique09@hotmail.com>
2024-03-12 21:33:32 -0300

    tabelafinanc

---
+++ javascript/Financ.js
@@ -0,0 +1,64 @@
+function CalcularFinanciamento() {
+    const emprestimo = parseFloat(document.getElementById("emprestimos").value);
+    const taxa = parseFloat(document.getElementById("taxa").value);
+    const parcelas = parseInt(document.getElementById("parcelas").value);
+    const entrada = parseFloat(document.getElementById("entrada").value);
+
+    const emprestimoFinal = emprestimo - entrada;
+
+    let resultado;
+
+    if (document.getElementById('sac').checked) {
+        resultado = calcularSAC(emprestimoFinal, taxa, parcelas);
+    } else {
+        resultado = calcularPRICE(emprestimoFinal, taxa, parcelas);
+    }
+
+    exibirResultado(resultado);
+}
+
+function calcularSAC(emprestimo, taxa, parcelas) {
+    let prestacoes = [];
+    let saldoDevedor = emprestimo;
+    const amortizacao = emprestimo / parcelas;
+
+    for (let i = 0; i < parcelas; i++) {
+        const juros = saldoDevedor * taxa / 100;
+        const totalPrestacao = amortizacao + juros;
+        saldoDevedor -= amortizacao;
+        prestacoes.push({ prestacao: totalPrestacao.toFixed(2), juros: juros.toFixed(2), amortizacao: amortizacao.toFixed(2), saldoDevedor: saldoDevedor.toFixed(2) });
+    }
+
+    return prestacoes;
+}
+
+function calcularPRICE(emprestimo, taxa, parcelas) {
+    let prestacoes = [];
+    let saldoDevedor = emprestimo;
+    const juros = Math.pow((1 + taxa / 100), parcelas);
+    const prestacao = emprestimo * (taxa / 100 * juros) / (juros - 1);
+
+    for (let i = 0; i < parcelas; i++) {
+        const jurosPrestacao = saldoDevedor * taxa / 100;
+        const amortizacaoPrestacao = prestacao - jurosPrestacao;
+        saldoDevedor -= amortizacaoPrestacao;
+        prestacoes.push({ prestacao: prestacao.toFixed(2), juros: jurosPrestacao.toFixed(2), amortizacao: amortizacaoPrestacao.toFixed(2), saldoDevedor: saldoDevedor.toFixed(2) });
+    }
+
+    return prestacoes;
+}
+
+function exibirResultado(resultado) {
+    const tabelaResultado = document.getElementById("tabelaResultado").getElementsByTagName('tbody')[0];
+    tabelaResultado.innerHTML = "";
+
+    let tipoJuros = document.querySelector('input[name="tipo-juros"]:checked').value;
+
+    let tabelaHTML = "<h2>" + tipoJuros.toUpperCase() + "</h2>";
+
+    resultado.forEach((item, index) => {
+        tabelaHTML += `<tr><td>${index + 1}</td><td>${item.prestacao}</td><td>${item.juros}</td><td>${item.amortizacao}</td><td>${item.saldoDevedor}</td></tr>`;
+    });
+
+    tabelaResultado.innerHTML = tabelaHTML;
+}
